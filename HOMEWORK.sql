e0f6945e17b854374c02f56aec7c067e10dbe19d
EAgha <41126467+EAghayev@users.noreply.github.com>
2024-02-14 21:49:36 +0400

    Add files via upload

---
+++ HOMEWORK.sql
@@ -0,0 +1,55 @@
+﻿CREATE DATABASE LIBRARY
+USE LIBRARY
+CREATE TABLE Books
+(
+Id Int,
+Name NVARCHAR(100),
+Price DECIMAL,
+)
+-- Books table-na Author column və Genre column əlavə etmək
+ALTER TABLE Books
+ADD Author NVARCHAR(10)
+ALTER TABLE Books
+ADD Genre NVARCHAR(10)
+SELECT*FROM Books
+
+ALTER TABLE Books
+ALTER COLUMN Author NVARCHAR(50)
+---Books table-dan Genre column silmek
+
+ALTER TABLE Books 
+DROP Column Genre
+
+Insert into Books(Id,Price,Author,Name)
+Values 
+(1,24,'Hikmet Abbasov','book1'),
+(2,12,'Tofiq Abbasov','book2'),
+(3,7.5,'Nermin Abbasova','book3'),
+(4,14.78,'Hikmet Abbasov','book4'),
+(5,70,'Elsen Qulamov','book5'),
+(6,43.7,'Elmar Qarayev','book6'),
+(7,71,'Davud Aliyev','book7'),
+(8,17,'Tofiq Quluzade','book8'),
+(9,7,'Qulu Abbasov','book9'),
+(10,7,'Nermine Quluzade','book10')
+
+UPDATE Books
+SET Price=10 
+WHERE Price<10
+
+SELECT * FROM Books
+WHERE Price BETWEEN 10 AND 50 
+
+SELECT * FROM Books
+WHERE Name LIKE '%Q%' OR Author LIKE '%q%' 
+
+DELETE FROM BOOKS
+WHERE PRICE=10
+
+select * from books
+where Price > all (select Price from books where price>0)
+
+
+
+
+
---
+++ task-subquery.sql
@@ -0,0 +1,70 @@
+﻿USE Course
+
+CREATE TABLE Employees
+(
+	Id INT PRIMARY KEY IDENTITY,
+	Name NVARCHAR(20) NOT NULL,
+	Surname NVARCHAR(20) NOT NULL CHECK(LEN(Surname)>=3),
+	Salary DECIMAL(18,2) CHECK(Salary>=350),
+	Email NVARCHAR(100)
+)
+
+ALTER TABLE Employees
+ADD CONSTRAINT UNQ_Email UNIQUE(Email)
+
+INSERT INTO Employees (Name,Surname,Email,Salary)
+VALUES
+(N'Tofiq',N'Abbasov','totu@gmail.com',1000),
+(N'Nərmin',N'Quluzadə','nermin@gmail.com',2300),
+(N'Nərminə',N'Quluyeva','nermine@gmail.com',2100),
+(N'Elmar',N'Qarayev','elmar@code.edu.az',350),
+(N'Davud',N'Əliyev','davud@code.com',400),
+(N'Dilbər',N'Nemətova','dilber@mail.ru',1200)
+
+SELECT * FROM Employees
+
+SELECT AVG(Salary) FROM Employees
+
+SELECT * FROM Employees
+WHERE Salary>(SELECT AVG(Salary) FROM Employees)
+
+SELECT MIN(Salary) FROM Employees
+
+UPDATE Employees
+SET Salary=350 WHERE Id=7
+
+SELECT * FROM Employees
+WHERE Salary = (SELECT MIN(Salary) FROM Employees) 
+
+SELECT SUBSTRING(Email,1,CHARINDEX('@',Email)-1) FROM Employees
+
+SELECT Id,CONCAT(Name,' ',Surname) AS Fullname,Salary FROM Employees AS E
+WHERE E.Salary>1000
+
+ALTER TABLE Employees
+ADD BirthDate DATETIME2
+
+UPDATE Employees
+SET BirthDate='1995-10-20 21:00'
+WHERE Id=1
+
+SELECT * FROM Employees
+WHERE BirthDate BETWEEN '1990-01-01' AND '2000-01-01'
+
+SELECT *,
+CONCAT(Name,' ',Surname) AS Fullname,
+MONTH(BirthDate)  AS BirthMonth,
+DATEDIFF(year, BirthDate, GETDATE()) AS Age1,
+YEAR(GETDATE())-YEAR(BirthDate) AS Age2 FROM Employees
+
+
+
+
+
+
+SELECT * FROM Employees
+
+DELETE FROM Employees
+
+TRUNCATE TABLE EMPLOYEES
+
